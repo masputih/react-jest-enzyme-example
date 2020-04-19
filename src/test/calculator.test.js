2b9345766e08d04f70e2f49321b05539e0947573
Anggie Bratadinata <anggie.bratadinata@gmail.com>
2020-04-19 21:15:02 +0700

    cleanup

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -3,18 +3,11 @@
  */
 
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { mount, shallow } from 'enzyme';
 
 import Calculator, { getSum } from '../components/calculator';
 
 describe('Calculator component', () => {
-  it('should render snapshot', () => {
-    const component = renderer.create(<Calculator />);
-
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
 
   it('should return the correct sum', () => {
     const sum = getSum(3, 5);
